1ca0971d65fd6d16f116ebb774255b1f54d1eb80
Bala-ks <balakuberan77@gmail.com>
2026-02-20 19:07:10 +0530

    Time: 3 ms (51.42%), Space: 43.5 MB (19.67%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.java
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -10,19 +10,19 @@ public class Solution {
             if (s.charAt(i) == '1') count++;
             else count--;
             
-            // when balanced, we found a special substring
+            
             if (count == 0) {
-                // recursively process inside part
+            
                 String inner = makeLargestSpecial(s.substring(start + 1, i));
                 list.add("1" + inner + "0");
                 start = i + 1;
             }
         }
         
-        // sort in descending order
+        
         Collections.sort(list, Collections.reverseOrder());
         
-        // combine all
+        
         StringBuilder result = new StringBuilder();
         for (String str : list) {
             result.append(str);
