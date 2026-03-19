a24a03e73f523b6f40146cbd50f5a403c2655fdc
Bala-ks <balakuberan77@gmail.com>
2026-03-19 14:51:47 +0530

    Time: 26 ms (5.55%), Space: 46.7 MB (21.34%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        if(n != t.length()) return false;
+        Map<Character,Integer> sm = new HashMap<>();
+        Map<Character,Integer> tm = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            char t1 = s.charAt(i);
+            char t2 = t.charAt(i);
+            sm.put(t1,sm.getOrDefault(t1,0)+1);
+            tm.put(t2,tm.getOrDefault(t2,0)+1);
+        }
+        return (sm.equals(tm))  ?  true :  false;
+    }
+}
