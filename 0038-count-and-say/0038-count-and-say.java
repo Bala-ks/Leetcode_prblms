01774f6a6c8286563e139f78ccaea69cd1ef0eee
Bala-ks <balakuberan77@gmail.com>
2026-03-28 19:21:44 +0530

    Time: 2 ms (99.63%), Space: 42.6 MB (98.59%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String countAndSay(int n) {
+
+        StringBuilder res = new StringBuilder();
+        res.append("1");
+        char curr = res.charAt(0);
+        for(int i = 1;i<n;i++){
+            res = rle(res);
+        }
+        return res.toString();
+    }
+
+    public StringBuilder rle(StringBuilder res){
+        int len = res.length();
+        StringBuilder t2 = new StringBuilder();
+        
+        for(int i = 0;i<len;i++){
+            int count = 1;
+            int temp = i+1;
+            char t3 = res.charAt(i);
+            while(temp < len && t3 == res.charAt(temp++)){
+                count++;
+            }
+            t2.append(count);
+            t2.append(t3);
+            i += count-1;
+        }
+    //    System.out.println(t2);
+        return t2;
+    }
+}
